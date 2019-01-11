778b288053bafa6dbc6bec617e271d7fa769f0f6
andereinmann <ander-eric.einmann@khk.ee>
2019-01-11 09:32:40 +0200

    muudetud index.php fail

---
+++ praks3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Ander-Eric Einmann'i veebileht</title>
+</head>
+<body>
+
+<h1>nimi: Ander-Eric Einmann</h1>
+<h1>rühm:ISP217</h1>
+<?php echo '<p>PHP is werking</p>'; ?>
+<p>e-mail: ander-eric.einmann@khk.ee</p>
+</body>
+</html>
